0e5f13d49563d6c3772c7065865a81e55e434ba2
dimas1318 <parshin@phystech.edu>
2018-11-29 00:32:42 +0300

    removed unused method

--- hw3/src/main/java/test/Test.java
+++ hw3/src/main/java/test/Test.java
@@ -2,7 +2,6 @@ package test;
 
 import counters.*;
 import org.openjdk.jmh.annotations.*;
-import org.openjdk.jmh.infra.Blackhole;
 import org.openjdk.jmh.results.format.ResultFormatType;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
@@ -37,11 +36,6 @@ public class Test {
      */
     private static final int MEASUREMENT_ITERATIONS = 70;
 
-    /**
-     * Number of spin iterations.
-     */
-    private final static int BUSY_SPIN_ITERATIONS = 100_000;
-
     @State(Scope.Benchmark)
     public static class CounterState {
         Counter synchronizedCounter = new SynchronizedCounter();
@@ -56,60 +50,54 @@ public class Test {
     @BenchmarkMode(Throughput)
     @OutputTimeUnit(SECONDS)
     @Group("SynchronizedCounter")
-    public void testSynchronizedCounter(CounterState state, Blackhole blackhole) {
+    public void testSynchronizedCounter(CounterState state) {
         state.synchronizedCounter.getNumber();
 //        sleep();
-        busyWait(blackhole);
     }
 
     @Benchmark
     @BenchmarkMode(Throughput)
     @OutputTimeUnit(SECONDS)
     @Group("ReentrantLockCounter")
-    public void testReentrantLockCounter(CounterState state, Blackhole blackhole) {
+    public void testReentrantLockCounter(CounterState state) {
         state.reentrantLockCounter.getNumber();
 //        sleep();
-        busyWait(blackhole);
     }
 
     @Benchmark
     @BenchmarkMode(Throughput)
     @OutputTimeUnit(SECONDS)
     @Group("AtomicLongCounter")
-    public void testAtomicLongCounter(CounterState state, Blackhole blackhole) {
+    public void testAtomicLongCounter(CounterState state) {
         state.atomicLongCounter.getNumber();
 //        sleep();
-        busyWait(blackhole);
     }
 
     @Benchmark
     @BenchmarkMode(Throughput)
     @OutputTimeUnit(SECONDS)
     @Group("VolatileCounter")
-    public void testVolatileCounter(CounterState state, Blackhole blackhole) {
+    public void testVolatileCounter(CounterState state) {
         state.volatileCounter.getNumber();
 //        sleep();
-        busyWait(blackhole);
     }
 
     @Benchmark
     @BenchmarkMode(Throughput)
     @OutputTimeUnit(SECONDS)
     @Group("SemaphoreCounter")
-    public void testSemaphoreCounter(CounterState state, Blackhole blackhole) {
+    public void testSemaphoreCounter(CounterState state) {
         state.semaphoreCounter.getNumber();
 //        sleep();
-        busyWait(blackhole);
     }
 
     @Benchmark
     @BenchmarkMode(Throughput)
     @OutputTimeUnit(SECONDS)
     @Group("StampedLockCounter")
-    public void testStampedLockCounter(CounterState state, Blackhole blackhole) {
+    public void testStampedLockCounter(CounterState state) {
         state.stampedLockCounter.getNumber();
 //        sleep();
-        busyWait(blackhole);
     }
 
     public static void main(String[] args) throws RunnerException {
@@ -120,7 +108,7 @@ public class Test {
                 .forks(1)
                 .threads(THREAD_COUNT)
                 .resultFormat(ResultFormatType.JSON)
-                .result("Threads" + THREAD_COUNT + "_W" + WARMUP_ITERATIONS + "_M" + MEASUREMENT_ITERATIONS + ".json")
+                .result("Threads" + THREAD_COUNT + "_W" + WARMUP_ITERATIONS + "_M" + MEASUREMENT_ITERATIONS + "sleep.json")
                 .build();
         new Runner(options).run();
     }
@@ -132,16 +120,6 @@ public class Test {
         try {
             Thread.sleep(Test.THREAD_SLEEP_TIME_MS);
         } catch (InterruptedException ignored) {
-        }
-    }
-
-    /**
-     * Busy spinning method.
-     * @param blackhole entity for consuming any values
-     */
-    private void busyWait(Blackhole blackhole) {
-        for (int i = 0; i < BUSY_SPIN_ITERATIONS; i++) {
-            blackhole.consume(i);
         }
     }
 }
